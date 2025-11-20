32a7b9cb99f55f5245484f3c0ecdfe331707f5c0
Ananthu-R-S <ananthuselvan@gmail.com>
2025-11-20 18:18:56 +0530

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,26 @@
+--  Step 1: Create Tables for Library Management
+
+CREATE TABLE Members (
+    member_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(15)
+);
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+    author VARCHAR(50),
+    category VARCHAR(50),
+    available BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE IssueRecords (
+    issue_id INT PRIMARY KEY AUTO_INCREMENT,
+    member_id INT,
+    book_id INT,
+    issue_date DATE,
+    return_date DATE,
+    FOREIGN KEY (member_id) REFERENCES Members(member_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
