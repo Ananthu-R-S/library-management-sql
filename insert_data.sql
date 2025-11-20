102ea9ee37aedd002383eb8e4b57b6f0de746d88
Ananthu-R-S <ananthuselvan@gmail.com>
2025-11-20 18:19:53 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,12 @@
+-- Step 2: Insert Sample Data
+
+INSERT INTO Members (name, email, phone)
+VALUES 
+('Ananthu', 'ananthu@mail.com', '9876543210'),
+('Rahul', 'rahul@mail.com', '9876500000');
+
+INSERT INTO Books (title, author, category)
+VALUES 
+('Java Basics', 'James Gosling', 'Programming'),
+('HTML5 & CSS3', 'Jon Duckett', 'Web Design'),
+('SQL Mastery', 'Joe Celko', 'Database');
