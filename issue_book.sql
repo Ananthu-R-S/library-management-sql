d25f97bd622e2b605a4d2ac58e670b7bdb462f08
Ananthu-R-S <ananthuselvan@gmail.com>
2025-11-20 18:20:52 +0530

    Create issue_book.sql

---
+++ issue_book.sql
@@ -0,0 +1,17 @@
+--  Step 3: Issue a Book
+
+INSERT INTO IssueRecords (member_id, book_id, issue_date)
+VALUES (1, 2, CURDATE());
+
+UPDATE Books
+SET available = FALSE
+WHERE book_id = 2;
+
+-- View issued books
+SELECT 
+    m.name AS Member, 
+    b.title AS Book, 
+    i.issue_date AS Issued_On
+FROM IssueRecords i
+JOIN Members m ON i.member_id = m.member_id
+JOIN Books b ON i.book_id = b.book_id;
